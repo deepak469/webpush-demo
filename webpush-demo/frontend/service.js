05aef4d6f622a1b7fd35fc1d59a8367747e2869e
Deepak Marneni <marnenideepak4@gmail.com>
2023-01-12 19:11:33 +0530

    Update service.js

--- webpush-demo/frontend/service.js
+++ webpush-demo/frontend/service.js
@@ -30,7 +30,7 @@ self.addEventListener("activate", async () => {
   // BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk
   try {
     const applicationServerKey = urlB64ToUint8Array(
-      "BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk"
+      "BIWh_y4-4dN9Nw5mKSSIvqjVUEPyorCtYqG90QsoWNjkv0wAGP9NVoQNW8SVea4_FDyHJttyl23ecBB9FpUnKA8"
     );
     const options = { applicationServerKey, userVisibleOnly: true };
     const subscription = await self.registration.pushManager.subscribe(options);
